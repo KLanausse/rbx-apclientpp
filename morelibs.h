4c4e0420e15cb356bd7a54d322a56025873d0374
Lanausse <lanausse@lanausse.com>
2026-06-28 19:39:12 -0400

    LoveMidi https://github.com/SiENcE/lovemidi/tree/master

--- morelibs.h
+++ morelibs.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <luau/lua.h>
 #include <luau/lualib.h>
+
 int luaopen_penguin(lua_State* L);
+int luaopen_luamidi(lua_State* L);
 
 static const luaL_Reg morelibs[] = {
     {"", luaopen_base},
     {"penguin", luaopen_penguin},
+    {"midi", luaopen_luamidi},
     {NULL, NULL},
 };
