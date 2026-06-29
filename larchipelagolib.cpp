4a4d8aab63b7eb1fc8672eeac7b7deb55e521a81
Lanausse <lanausse@lanausse.com>
2026-06-29 18:07:08 -0400

    Fix build params

--- larchipelagolib.cpp
+++ larchipelagolib.cpp
@@ -1,5 +1,37 @@
+#ifdef _WIN32
+// we currently don't do project-wide defines for msvc, so define the relevant ones here
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef ASIO_STANDALONE
+#define ASIO_STANDALONE
+#endif
+#endif
+
+
+//#define APCLIENT_DEBUG // to get debug output
+
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpragmas" // gcc doesn't know -Wunknown-warning-option
+#pragma GCC diagnostic ignored "-Wunknown-warning-option" // clang doesn't know -Wtemplate-id-cdtor
+#pragma GCC diagnostic ignored "-Wtemplate-id-cdtor"
+#pragma GCC diagnostic ignored "-Wnull-pointer-subtraction"
+#pragma GCC diagnostic ignored "-Wdeprecated-literal-operator"
+#ifdef _WIN32
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#endif
+#endif
+
 #include <luau/lua.h>
 #include <luau/lualib.h>
+
+#include <apclient.hpp>
+#include <luacompat.h>
+#include <luapp.h>
+#include <luaref.h>
+#include <lua_json.h>
 
 static int coolCustomFunctionThatIMade(lua_State* L)
 {
